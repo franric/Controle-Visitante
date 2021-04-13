2496d63759a370e3e81b6df8e819019629375e52
Francisco Ricardo <78240032+franRic@users.noreply.github.com>
2021-04-13 17:55:17 -0400

    Mostrando menssagens de error

--- visitantes/views.py
+++ visitantes/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from visitantes.forms import VisitanteForm
 
 
@@ -9,7 +10,17 @@ def registrar_visitantes(request):
         form =VisitanteForm(request.POST)
 
         if form.is_valid():
-            form.save()
+            visitante = form.save(commit=False)
+
+            visitante.registrado_por = request.user.porteiro
+            visitante.save()
+
+            messages.success(
+                request,
+                "Visitante Registrado com Sucesso"
+            )
+
+            return redirect("index")
 
     context = {
         "nome_pagina": "Registrar Visitante",
